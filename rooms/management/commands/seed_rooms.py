b3af1b03cfac7459eb6762561caa82c7d48d537d
ktkim <ktkim0111@gmail.com>
2022-03-08 18:03:09 +0900

    values() dict value 이거 뭐야

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,3 +1,4 @@
+import random
 from django.core.management.base import BaseCommand
 from django_seed import Seed
 from rooms import models as room_models
@@ -19,7 +20,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         number = options.get("number")
         seeder = Seed.seeder()
-        all_user = user_models.User.objects.all()
-        seeder.add_entity(room_models.Room, number)
-        seeder.execute()
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "price": lambda x: random.randint(1, 300),
+                "guests": lambda x: random.randint(1, 20),
+                "beds": lambda x: random.randint(1, 5),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 5),
+            },
+        )
+        created_photos = seeder.execute()
+        print(created_photos.values())
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
