a4069bccc26a6f906df41236b5279bf4d83bf987
ktkim <ktkim0111@gmail.com>
2022-03-16 19:08:37 +0900

    login form 할 차례

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path(
+        "login/",
+        views.LoginView.as_view(),
+        name="login",
+    )
+]
