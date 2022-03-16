a19a2aec2b6f0332d65e9a425f1c3981fb6a0e72
ktkim <ktkim0111@gmail.com>
2022-03-16 22:50:41 +0900

    email, password 맞다는 걸 확인했을 뿐 로그인 시킨 건 아직 아님

--- users/views.py
+++ users/views.py
@@ -1,10 +1,34 @@
 from django.views import View
 from django.shortcuts import render
+from . import forms
 
 
 class LoginView(View):
+    # 페이지에 접근했을 때
     def get(self, request):
-        return render(request, "users/login.html")
+        form = forms.LoginForm(
+            initial={
+                "email": "itn@las.com",
+            }
+        )
+        return render(
+            request,
+            "users/login.html",
+            {
+                "form": form,
+            },
+        )
 
+    # Login 요청했을 때
+    # clean - dict형태로 정리해 줌 - clean_email 등의 함수의 return 값으로
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(
+            request,
+            "users/login.html",
+            {
+                "form": form,
+            },
+        )
