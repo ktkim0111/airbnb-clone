40be6b7c089f6f52c5b313dcc019b9e1b3c91109
ktkim <ktkim0111@gmail.com>
2022-03-17 12:31:03 +0900

    Model form 할 차례

--- users/urls.py
+++ users/urls.py
@@ -4,14 +4,7 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path(
-        "login/",
-        views.LoginView.as_view(),
-        name="login",
-    ),
-    path(
-        "logout/",
-        views.log_out,
-        name="logout",
-    ),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.log_out, name="logout"),
+    path("signup/", views.SingUpView.as_view(), name="signup"),
 ]
