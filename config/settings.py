b4be235ae020ef2eb3067a4158ecdde3f16cac14
ktkim <ktkim0111@gmail.com>
2022-03-18 23:12:30 +0900

    email verify done

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "sexy-guy@sandboxa36a0face3f8415d8a90006d2e1d382e.mailgun.org"
